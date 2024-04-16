2c6b3d31431dcd59172096f3a6f8f81a5347259c
KhaledAlabssi <khaled.absi5@gmail.com>
2024-04-16 12:27:56 +0200

    add routers and update the home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-7xl'>Hello</h1>
+    <div data-theme="nord" className="min-h-screen min-w-screen flex flex-col">
+      {/* navbar */}
+      <div className="navbar bg-base-300 ">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+
+      {/* body/content */}
+      <div className='flex-grow'>
+        <Routes>
+          <Route path='/' element={<h1>Home page</h1> } />
+          <Route path='/cli' element={<h1>Cli page</h1> } />
+
+        </Routes>
+
+
+      </div>
+
+      {/* footer */}
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>Copyright © 2024 - All right reserved Testination.net</p>
+        </aside>
+      </footer>
     </div>
   );
 }
